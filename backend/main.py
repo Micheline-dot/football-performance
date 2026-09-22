39f46c2b41ee2b70465e8efa880c15493969307a
Micheline-dot <michelineclearevillagomezcastr@gmail.com>
2026-09-22 07:11:55 -0400

    Corregir CORS

--- backend/main.py
+++ backend/main.py
@@ -64,12 +64,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://football-performance-frontend.onrender.com"
+],
 )
 
 
